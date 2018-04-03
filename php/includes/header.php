f5e952f96543fbdb733e2b2abf79d75e1eedec7c
olemartin93 <ohelgesen39@gmail.com>
2018-04-03 13:22:34 +0200

    endret css, samt lagt til meta-tag

--- php/includes/header.php
+++ php/includes/header.php
@@ -5,6 +5,10 @@
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--Lag meta-tag forskjellig basert på hvilken sub-page man er på -->
+    <meta name="description" content="This website show how an international student lived during my study abroad trip to Australia, 
+    which includes my study vacation to gold coast
+    and my after semester trip to Fiji. It also gives some examples of how the syllabus was at my uni."> 
     <link rel="shortcut icon" href="../img/favicon.ico" type="favicon/ico"/>
     <link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css">
     <title>Australia</title>
