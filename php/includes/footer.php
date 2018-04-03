6c39809b7f47708acec6bce75cbe91c371752ca7
olemartin93 <ohelgesen39@gmail.com>
2018-04-03 02:03:01 +0200

    startet på ny footer med dynamisk connections

--- php/includes/footer.php
+++ php/includes/footer.php
@@ -7,20 +7,72 @@
 			<br>
 			<p><i class="fa fa-user-circle" aria-hidden="true"></i> Ole Martin Helgesen</p>
 			<p><i class="fa fa-envelope" aria-hidden="true"></i><a href="ohelgesen39@gmail.com" target="_top"> ohelgesen39@gmail.com</a></p>
-			<p><i class="fa fa-phone" aria-hidden="true"></i> (+47) 93842341</p><br>
-
-
-
-
-				
+			<p><i class="fa fa-phone" aria-hidden="true"></i> (+47) 93842341</p><br>	
 			</div>
-			<div class="col-sm-4">
+
+
+		<!--mid section - connect -->
+		<ul class="accordion">
+            <li class="tab">
+                <div class="social instagram">
+                    <a
+                     href="https://www.instagram.com/olemartinhel/" target="_blank">instagram</a>
+                </div>
+                <div class="content">
+                    <h1>Instagram</h1>
+                    <p>Follow me on Instagram to keep up with my latest adventures</p>
+                </div>
+            </li>
+
+            <li class="tab">
+                    <div class="social vimeo">
+                        <a href="https://vimeo.com/user72789995" target="_blank">Vimeo</a>
+                    </div>
+                    <div class="content">
+                        <h1>Vimeo</h1>
+                        <p>Check out my vimeo to see my latest videos - currently working on content to upload</p>
+                    </div>
+                </li>
+
+                <li class="tab">
+                        <div class="social facebook">
+                            <a href="https://www.facebook.com/olemart1n" target="_blank">facebook</a>
+                        </div>
+                        <div class="content">
+                            <h1>facebook</h1>
+                            <p>Feel free to send me a message if it's anything you would like to know</p>
+                        </div>
+                    </li>
+
+                    <li class="tab">
+                            <div class="social linkedin">
+                                <a href="https://www.linkedin.com/in/ole-martin-helgesen-28a4a4b7/" target="_blank">linkedin</a>
+                            </div>
+                            <div class="content">
+                                <h1>linkedin</h1>
+                                <p>Connect with me on a professional level</p>
+                            </div>
+                        </li>
+
+                        <li class="tab">
+                            <div class="social github">
+                                <a href="https://github.com/olemartin93" target="_blank">github</a>
+                            </div>
+                            <div class="content">
+                                <h1>github</h1>
+                                <p>Check out my Github to se some of my projects I have worked on</p>
+                            </div>
+                        </li>
+        </ul>
+
+			<!-- <div class="col-sm-4">
 				<h3>Connect</h3>
 				<a href="https://www.instagram.com/olemartinhel/" class="fa fa-instagram fa-3x" target="_blank"></a>
 				<a href="https://www.facebook.com/olemart1n" class="fa fa-facebook fa-3x" target="_blank"></a>
 				<a href="https://www.linkedin.com/in/ole-martin-helgesen-28a4a4b7/" class="fa fa-linkedin fa-3x" target="_blank"></a>
 				<a href="https://vimeo.com/user72789995" class="fa fa-vimeo fa-3x" target="_blank"></a>
-			</div>
+			</div> -->
+
 			<div class="col-sm-4">
 				<br>
 				<blockquote class="blockquote-reverse"><p>The world is a book, and those who do not travel read only a page</p><footer>Quote by <cite titlte="Saint Augustine">Saint Augustine</cite></footer> </blockquote>
