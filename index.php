f68e5bfcb6e3c1f3d6ef2e1bfbcf6ff4a80aa990
Ole Martin Helgesen <ohelgesen39@gmail.com>
2018-04-27 02:18:11 +0200

    forsøk på å rydde litt i koden, slik det er lettere å fortsettere senere

--- index.php
+++ index.php
@@ -1,10 +1,81 @@
 <?php
 // code for <head>
 require_once('php/includes/indexHeader.php');
+?>
+<nav class="navbar navbar-default navbar-inverse">
+<div class="container-fluid">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="index.html"><img src="img/logo1.png"></a>
+  </div>
 
-//code for <body>
-require_once('php/pages/indexBody.php');
 
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="html/studyVacation.html">Study Vacation</a></li>
+        <li><a href="#">Australia</a></li>
+     <li><a href="#">Fiji</a></li>
+      <li><a href="#">About me</a></li>
+      
+
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Subjects<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="php/InteractiveMedia.php">Interactive Media</a></li>
+          <li><a href="#">Networked Enterprise Architecture</a></li>
+          <li><a href="#">Object-relational Databases</a></li>
+          
+          <li><a href="academicEnglish.html">Academic English</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</div><!-- /.container-fluid -->
+</nav>
+
+
+<!-- INTRODUCTION -->
+<div class="jumbotron">
+  <div class="container">
+      <div class="row">
+      <h1>Welcome to my webpage</h1>
+      <p>Hello, on 18 July started I my adventure down under, in Australia.<a href="#hidden" data-toggle="collapse">Read more</a></p>
+      <div id="hidden" class="collapse">
+          <p>After I got there</p>
+      </div>
+      </div>
+  </div>
+</div>
+
+
+<div class="container">
+  <div class="row">
+      <div class="col-md-6">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+      <div class="col-md-3">
+          <img src="img/profileBondi.jpg" class="img-responsive" alt="Responsive image">
+      </div>
+              <div class="col-md-3">
+          <img src="img/profileTaronga.jpg" class="img-responsive" alt="Responsive image">
+      </div>
+
+  </div>
+</div>
+<?php
 // code for footer
 require_once('php/includes/indexFooter.php');
 ?>
